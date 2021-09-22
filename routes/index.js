2d95a052298415b056144c5dfc25cef02f498df3
Eduardo87h7 <eduardogarciar160998@gmail.com>
2021-09-22 12:58:55 -0500

    agreganndo nombre de compañia

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author:'EDUARDO', appName: 'webApp' });
+  res.render('index', { title: 'Express', author:'EDUARDO', appName: 'webApp', company:'Awsom Software' });
 });
 /*AGREGANDO NUEVA RUTA */ 
 /*
