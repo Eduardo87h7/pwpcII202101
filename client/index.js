2bc8df283fafe6b109d663da301ef4057a812497
Eduardo87h7 <eduardogarciar160998@gmail.com>
2021-10-15 11:14:04 -0500

    Guia de estilos de Airbnb

--- client/index.js
+++ client/index.js
@@ -1,25 +1,27 @@
-import './stylesheets/style.css'
-import './stylesheets/mystyles.css'
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import './stylesheets/style.css';
+import './stylesheets/mystyles.css';
 
-console.log("webpack Working!!!");
-//Defautl Parametros ES6/2015
-let show =(m="Hot Modules Remplacement Workin") => {
-    alert(m)
+console.log('webpack Working!!!');
+// Defautl Parametros ES6/2015
+const show = (m = 'Hot Modules Remplacement Workin') => {
+  alert(m);
 };
 show();
 
-function resolveafter25seconds(){
-    return new Promise(resolve=>{
-        setTimeout(()=>{
-            resolve('resolved')
-        },2000);
-    });
+function resolveafter25seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
 }
 
-async function asyncCall(){
-    console.log("Calling an async function");
-    const result = await resolveafter25seconds();
-    console.log(result);
+async function asyncCall() {
+  console.log('Calling an async function');
+  const result = await resolveafter25seconds();
+  console.log(result);
 }
 
 asyncCall();
