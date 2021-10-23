c0f53cae899e1c6207799c9d57c4674861f095a9
Eduardo87h7 <eduardogarciar160998@gmail.com>
2021-10-23 08:17:10 -0500

    renovando Codigo www.js

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,40 +4,20 @@ import winston from '@server/config/winston';
 /**
  * Module dependencies.
  */
+import Debug from 'debug';
+import http from 'http';
+import app from '../app';
 
-var app = require('../app');
-var debug = require('debug')('generator:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const debug = Debug('debug')('projnotes:server');
 
 /**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
+ * http server.
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
+  if (Number.isNaN(port)) {
     // named pipe
     return val;
   }
@@ -49,6 +29,13 @@ function normalizePort(val) {
 
   return false;
 }
+/**
+ * Get port from environment and store in Express.
+ */
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
 
 /**
  * Event listener for HTTP server "error" event.
@@ -59,19 +46,13 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      
       winston.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      
       winston.error(`${bind} is already in use`);
       process.exit(1);
       break;
@@ -81,13 +62,23 @@ function onError(error) {
 }
 
 /**
- * Event listener for HTTP server "listening" event.
+ * Create HTTP server.
  */
 
+const server = http.createServer(app);
+
+
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  const addr = server.address();
+  const bindAdr =
+    typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bindAdr}`);
 }
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
