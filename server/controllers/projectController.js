484a581963764dbb4046b2c2084da5b039358adf
Eduardo87h7 <eduardogarciar160998@gmail.com>
2021-11-12 11:33:31 -0600

    RUTAS PARA AGREGAR Y LISTAR FUNCIONANDO

---
+++ server/controllers/projectController.js
@@ -0,0 +1,13 @@
+const index = (req, res) => {
+  res.send('Respondiendo a "/Projects/index"');
+};
+  
+// "/projects/add"
+const add = (req, res) => {
+  res.send('Respondiendo a "/projects/add"');
+};
+// Pendiente por programar
+export default {
+  add,
+  index,
+};
---
+++ server/routes/project.js
@@ -0,0 +1,13 @@
+// Importar Router
+import { Router } from 'express';
+
+import projectController from '@server/controllers/projectController';
+// Creando la Instancia de Router
+const router = new Router();
+// "/projects" "/projects/index"
+router.get(['/', '/index'], projectController.index);
+// "/projects/add"
+// sirve el formulario para agregar proyectos
+router.get('/add', projectController.add);
+
+export default router;
