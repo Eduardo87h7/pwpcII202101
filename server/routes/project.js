366562f889489d6c4f45e5c4482f3c9e725d4bbe
Eduardo87h7 <eduardogarciar160998@gmail.com>
2021-11-29 07:37:40 -0600

    procesando el formulario

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,13 +1,21 @@
 // Importar Router
 import { Router } from 'express';
 
+//  Importar el controlador de proyectos
 import projectController from '@server/controllers/projectController';
-// Creando la Instancia de Router
+
+// Creando Instancia de Router
 const router = new Router();
-// "/projects" "/projects/index"
+
+// GET "/projects" "/projects/index"
 router.get(['/', '/index'], projectController.index);
-// "/projects/add"
+
+// GET "/projects/add"
 // sirve el formulario para agregar proyectos
 router.get('/add', projectController.add);
+
+// POST "/projects/add"
+// Procesa el formulario
+router.post('/add', projectController.addPost);
 
 export default router;
